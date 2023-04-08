4e94038adf45860d723d93a2b868cc9fda7ff233
rijan <rijanadhikari@gmail.com>
2023-04-08 18:14:53 +0900

    refactor: update books page

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,36 @@
 <template>
-  <h2>hello</h2>
+  <n-space vertical>
+    <n-button size="large" type="primary" :render-icon="renderIcon">
+      Register new book
+    </n-button>
+    <div class="books">
+      <n-space>
+        <BookCard />
+        <BookCard />
+        <BookCard />
+        <BookCard />
+        <BookCard />
+      </n-space>
+    </div>
+  </n-space>
 </template>
+
+<script lang="ts">
+import { defineComponent, h } from 'vue'
+import { NSpace, NButton } from 'naive-ui'
+import { BookAdd20Filled } from '@vicons/fluent'
+export default defineComponent({
+  components: {
+    BookAdd20Filled,
+    NButton,
+    NSpace,
+  },
+  setup() {
+    return {
+      renderIcon() {
+        return h(BookAdd20Filled)
+      },
+    }
+  },
+})
+</script>
