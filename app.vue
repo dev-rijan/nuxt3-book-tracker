f2486093cc464c8daa00e66ea1c44cbc21947d94
rijan <rijanadhikari@gmail.com>
2023-04-08 14:54:19 +0900

    refactor: app structure

--- app.vue
+++ app.vue
@@ -1,6 +1,13 @@
 <template>
-  <div>
-    <NuxtWelcome />
-    <h1>hello</h1>
-  </div>
+  <n-config-provider>
+    <n-message-provider>
+      <NuxtLayout>
+        <NuxtLoadingIndicator :height="5" :duration="3000" :throttle="400" />
+        <NuxtPage />
+      </NuxtLayout>
+    </n-message-provider>
+  </n-config-provider>
 </template>
+<script lang="ts" setup>
+import { NConfigProvider, NMessageProvider } from 'naive-ui'
+</script>
